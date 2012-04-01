b0d88ff27426d186212d44b9cef1561215066c2c
Corey Oliver <corey.jon.oliver@gmail.com>
2012-04-01 16:37:14 -0500

    changed font size

--- custom/my-ui.el
+++ custom/my-ui.el
@@ -40,7 +40,7 @@
 ;; system fonts are available in emacs as of version 23
 (if (>= emacs-major-version 23)
     (set-face-attribute 'default nil
-			:family "Inconsolata" :height 120))
+			:family "Inconsolata" :height 190))
 
 ;; setup color-theme
 (require 'color-theme)
